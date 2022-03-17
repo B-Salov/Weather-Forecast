a037172c7b767681f36ba1e5123cdd52c785d49b
Bogdan <jon40292@gamil.com>
2022-03-17 13:56:16 +0200

    Add data storage

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 """WEATHER FORECAST
-This program get current weather
+This program get current weather and write it in a storage.
+If the data is more than one day old, then it is overwritten.
 
   --Project Plan--
-* Create time limits in storage
 * Weather forecast for a couple of days
 * Create output style
 """
 import json
 import requests
 from pprint import pprint
+from datetime import datetime
 
 
 class OpenWeatherForecast:
@@ -17,7 +18,6 @@ class OpenWeatherForecast:
         self.city = city
 
     def create_request(self):
-        print("Making http request!")
         url = f'http://api.openweathermap.org/data/2.5/weather?q={self.city}' \
               '&units=metric&appid=9f886e7aea62aa740e5aa61a638a572d'
 
@@ -29,60 +29,88 @@ class OpenWeatherForecast:
 
 class CacheData:
 
-    def __init__(self, city):
+    def __init__(self, city, storage):
         self.city = city
+        self.storage = storage
+        self.get_forecast = OpenWeatherForecast(self.city)
 
-    def check_in_storage(self):
-        data = CacheData.load_data()
+    def get_cache(self):
+        data = CacheData.load_data(self)
+
         if data:
-            for i in data:
-                if self.city in i:
-                    return i[self.city]
+            new_data = self.get_forecast.create_request()
+            res = {self.city: new_data, 'time': str(datetime.now())}
+            
+            city_exist = [i for i in data if self.city in i]
 
-        return None
+            if city_exist:
+                if self.check_time(city_exist[0]['time']):
+                    return city_exist[0]
+                else:
+                    result = []
+                    for row in data:
+                        if self.city in row:
+                            result.append(res)
+                            continue
 
-    def write_data(self, forecast):
-        data = CacheData.load_data()
+                        result.append(row)
 
-        if not data:
-            data = [{self.city: forecast}]
-        else:
-            data.append({self.city: forecast})
+                    CacheData.write_data(self, result)
 
-        with open('storage.json', 'w') as f:
+            else:
+                data.append(res)
+                CacheData.write_data(self, data)
+
+            return res
+
+    def write_data(self, data):
+        with open(self.storage, 'w') as f:
             json.dump(data, f, indent=2)
 
-    @staticmethod
-    def load_data():
+    def load_data(self):
         try:
-            data = json.load(open('storage.json'))
+            data = json.load(open(self.storage))
         except json.decoder.JSONDecodeError:
-            return None
+            data = None
+        except FileNotFoundError:
+            data = None
 
         return data
+
+    @staticmethod
+    def check_time(time):
+        creation_time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
+        delta_time = datetime.now() - creation_time
+
+        return True if delta_time.days == 0 else False
 
 
 class CityInfo:
 
-    def __init__(self, city):
+    def __init__(self, city, storage):
         self.city = city
         self._get_weather = OpenWeatherForecast(city)
-        self._cache_data = CacheData(city)
+        self._cache_data = CacheData(city, storage)
 
     def get_forecast(self):
-        weather = self._cache_data.check_in_storage()
+        weather = self._cache_data.get_cache()
 
         if not weather:
-            forecast = self._get_weather.create_request()
-            self._cache_data.write_data(forecast)
+            new_data = self._get_weather.create_request()
+            first_record = [{self.city: new_data, 'time': str(datetime.now())}]
 
-            return forecast
+            self._cache_data.write_data(first_record)
+
+            return new_data
 
         return weather
 
 
 def _main():
-    city_info = CityInfo('Gamburg')
+    city = input('Your city --> ')
+    storage = 'storage.json'
+
+    city_info = CityInfo(city, storage)
     weather = city_info.get_forecast()
 
     pprint(weather)
